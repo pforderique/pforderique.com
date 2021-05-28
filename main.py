3078eeeb163e4b102f1a51f05e6c7527f4a3b82c
ianmalcolm93 <drmalcolm93@gmail.com>
2021-05-28 15:42:24 -0400

    main file to port 8000

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000) 
+    app.run(host="0.0.0.0", debug=True, port=8000) 
